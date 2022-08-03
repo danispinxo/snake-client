400b20bb266cc61e2bb07c45d44cbeef26706c90
danispinxo <genericpronoun@gmail.com>
2022-08-03 20:11:35 +0000

    added event listener for stdin data and CTL-C to close

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 const net = require("net");
+
 const connect = function () {
   const conn = net.createConnection({
     host: "165.227.47.243", // IP address here,
@@ -10,19 +11,19 @@ const connect = function () {
   conn.on("connect", () => {
     console.log("Successfully connected to game server.");
     conn.write("Name: DS");
-
-    setInterval(() => {
-      conn.write("Move: up");
-    }, 1000);
-
+    // setInterval(() => {
+    //   conn.write("Move: up");
+    // }, 1000);
   })
-
 
   conn.on("data", (data) => {
     console.log(data);
   });
 
   return conn;
+
 };
 
-module.exports = connect;
+module.exports = {
+  connect
+};
--- play.js
+++ play.js
@@ -1,5 +1,28 @@
 const net = require("net");
-const connect = require("./client");
+const client = require("./client");
 
 console.log("Connecting ...");
-connect();
+client.connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+const handleUserInput = function (data) {
+  
+  console.log(data);
+
+  if (data === '\u0003') {
+    process.exit();
+  }
+
+};
+
+setupInput();
