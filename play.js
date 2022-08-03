6cfa6372989eb366acb8c71ee45920a55302cd06
danispinxo <genericpronoun@gmail.com>
2022-08-03 20:21:23 +0000

    adding input.js

--- play.js
+++ play.js
@@ -1,28 +1,8 @@
 const net = require("net");
-const client = require("./client");
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
-client.connect();
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
-
-const handleUserInput = function (data) {
-  
-  console.log(data);
-
-  if (data === '\u0003') {
-    process.exit();
-  }
-
-};
+connect();
 
 setupInput();
